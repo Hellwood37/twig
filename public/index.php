76af8ea08fe11f8a297dc8977b5cdbb1e41125fc
Etienne Chevalier <youpigod@hotmail.fr>
2019-10-22 14:41:44 +0200

    Résolu

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
-
-$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
-
 require __DIR__.'/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
 $twig = new Twig\Environment($loader);
-echo $twig->render('index.html.twig', ['products'=>$products]);
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 
-
+echo $twig->render('index.html.twig', array 
+    (
+        'products' => array('banane', 'fraise', 'citron', 'abricot', 'framboise')
+    )
+);
 
 ?>
 
