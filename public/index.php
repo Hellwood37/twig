33188c6a740ed0c23ebaae809289faa171134d72
Etienne Chevalier <youpigod@hotmail.fr>
2019-10-22 14:55:49 +0200

    Résolu2

--- public/index.php
+++ public/index.php
@@ -2,11 +2,10 @@
 require __DIR__.'/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
 $twig = new Twig\Environment($loader);
-$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 
 echo $twig->render('index.html.twig', array 
     (
-        'products' => array('banane', 'fraise', 'citron', 'abricot', 'framboise')
+        'products' => array('banane', 'fraise', 'citron', 'abricot', 'framboise', 'fenouil')
     )
 );
 
